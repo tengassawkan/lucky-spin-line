53768f54dc8a9dede67838ba02cc2038d6a37101
tengassawkan <tengassawkan@gmail.com>
2025-08-08 15:03:00 +0700

    Fix webhook code and lucky spin logic

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ app.use(bodyParser.json());
 const CHANNEL_ACCESS_TOKEN = 'N9MdAkeCqg6kMk2LgwkTl6dy9yhba10ec4l9w5APzRy3SpSfZlur4dfDtQ/CUVQa2p16LaE1kpyGOgOO9jzYy8q5ouh1o+J19/hIQTmPzyEaSMOI3Dh/SJjytIoFm0j5IOT3S/ommuDPGpuXcE4GNQdB04t89/1O/w1cDnyilFU=';
 const LIFF_URL = 'https://lucky-spin-line.onrender.com/lucky-spin.html';  // เปลี่ยนเป็นลิงก์เว็บวงล้อหมุนของคุณ
 
+const path = require('path');
+app.use(express.static(path.join(__dirname, 'public')));
+
 // เก็บสถานะผู้ใช้รอ confirm
 const waitingForConfirm = new Set();
 
