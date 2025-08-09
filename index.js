e3fe674a95a9407bb973e4b55740ac9892677283
tengassawkan <tengassawkan@gmail.com>
2025-08-09 22:33:14 +0700

    update lucky spin code

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ const CHANNEL_ACCESS_TOKEN = 'N9MdAkeCqg6kMk2LgwkTl6dy9yhba10ec4l9w5APzRy3SpSfZl
 
 // รางวัลและน้ำหนักความน่าจะเป็น
 const weightedPrizes = [
-  { prize: { text: '🎉 โซฟาปรับนอน Recliner มูลค่า 8,900 -!', image: 'https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExZTN6ZG0yMjk4bHZ1b3NpcDgwYnE2bmViNTlqZ2VwcDdiZmgyOGNsZSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/Wx0CKQMmqPMadG4VUs/giphy.gif' }, weight: 1 },  // โอกาสออกน้อยที่สุด
   { prize: { text: '🎉 ชุดผ้าปู + ปลอกหมอน 6 ฟุค มูลค่า 790 -', image: 'https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExZTN6ZG0yMjk4bHZ1b3NpcDgwYnE2bmViNTlqZ2VwcDdiZmgyOGNsZSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/Wx0CKQMmqPMadG4VUs/giphy.gif' }, weight: 5 },
   { prize: { text: '🎉 หมอนหนุน มูลค่า 250 - !', image: 'https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExZTN6ZG0yMjk4bHZ1b3NpcDgwYnE2bmViNTlqZ2VwcDdiZmgyOGNsZSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/Wx0CKQMmqPMadG4VUs/giphy.gif' }, weight: 5 },
   { prize: { text: '🎉 หมอนข้าง มูลค่า 350 - !', image: 'https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExZTN6ZG0yMjk4bHZ1b3NpcDgwYnE2bmViNTlqZ2VwcDdiZmgyOGNsZSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/Wx0CKQMmqPMadG4VUs/giphy.gif' }, weight: 5 }
